052351690f2a1369fc10a31b9e4da5f37c4679a3
Yassoss <yassine.meddiki@gmail.com>
2024-02-13 17:25:19 +0100

    Terzo commit

--- src/main/java/com/mycompany/granprix_n2_5ainf/Pilota.java
+++ src/main/java/com/mycompany/granprix_n2_5ainf/Pilota.java
@@ -1,6 +1,9 @@
 package com.mycompany.granprix_n2_5ainf;
 
 public class Pilota {
-    int matricola;
     String password;
+    int matricola;
+    String macchina;
+
+    //Inserire i metodi "accedi" e "scegli auto"
 }
